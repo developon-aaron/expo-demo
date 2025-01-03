af398d5ebae893945ead080f10c72bef17632423
Kanghyeon Park <130806999+developon-aaron@users.noreply.github.com>
2025-01-03 20:44:53 +0900

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,136 @@
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { Link } from "expo-router";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+//Schema
+
+const validationSchema = Yup.object().shape({
+  username: Yup.string()
+    .required("Username is required")
+    .matches(
+      /^[a-zA-Z0-9]+$/,
+      "Username can only contain alphanumeric characters"
+    )
+    .min(6),
+
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters long")
+    .required("Password must be at least 8 characters long."),
+});
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>KMLA Online</Text>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log("Form submitted:", values);
+        }}
+        validationSchema={validationSchema}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+        }) => (
+          <View style={styles.formContainer}>
+            {/* Username Input */}
+            <TextInput
+              style={styles.input}
+              placeholder="Username"
+              onChangeText={handleChange("username")}
+              onBlur={handleBlur("username")}
+              value={values.username}
+            />
+            {touched.username && errors.username && (
+              <Text style={styles.errorText}>{errors.username}</Text>
+            )}
+
+            {/* Password Input */}
+            <TextInput
+              style={styles.input}
+              placeholder="Password"
+              secureTextEntry
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+            />
+            {touched.password && errors.password && (
+              <Text style={styles.errorText}>{errors.password}</Text>
+            )}
+
+            {/* Submit Button */}
+            <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+              <Text style={styles.buttonText}>로그인</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Link href="./auth/register" style={styles.buttonText}>
+            회원가입
+          </Link>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  formContainer: {
+    width: "80%",
+    marginBottom: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingLeft: 10,
+    borderRadius: 5,
+  },
+  errorText: {
+    color: "red",
+    fontSize: 12,
+    marginBottom: 10,
+  },
+  buttonContainer: {
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: "#6200ea",
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    elevation: 3, // Adds a shadow effect on Android
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
